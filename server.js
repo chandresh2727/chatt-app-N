1fd85b14242606a8926a2840e7c4ca9637b6e6e6
chandresh2727 <trivedichandresh882@gmail.com>
2023-09-11 08:22:03 +0530

    Added some new feature

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.on('connection', (socket) => {
+  console.log(`User connected: ${socket.id}`);
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg); // Broadcast the message to all connected clients
+  });
+
+  socket.on('disconnect', () => {
+    console.log(`User disconnected: ${socket.id}`);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
